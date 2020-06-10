c3b34839d5cf1772db0e6359d71a40f495f9938f
paweltar <paweltar@gmail.com>
2020-06-10 11:15:03 +0200

    Make related case studies cards dynamic

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,17 @@ module.exports = function (eleventyConfig) {
     // Copy `favicon/` to `_site/favicon`
     eleventyConfig.addPassthroughCopy("source/favicon");
 
+    // limit filter
+    eleventyConfig.addFilter("limit", function (array, limit) {
+        return array.slice(0, limit);
+    });
+
+    // exclude filter
+    // eleventyConfig.addFilter("exclude", function (array, post_url) {
+    //     var filtered = array.filter(function (value, index, arr) { return value !== this.post.url; });
+    //     return filtered;
+    // });
+
     // You can return your Config object (optional).
     return {
         passthroughFileCopy: true,
