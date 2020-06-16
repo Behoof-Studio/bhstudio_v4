ccb483b668bdd261dddd7b9f977371d94389eb2b
paweltar <paweltar@gmail.com>
2020-06-16 11:33:46 +0200

    Connect with Sanity - initial setup

---
+++ source/_data/posts.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const sanityClient = require('@sanity/client')
+
+const projectId = process.env.SANITY_PROJECT
+// const apiToken = process.env.SANITY_TOKEN
+
+const client = sanityClient({
+    projectId,
+    dataset: 'production',
+    token: '', // sanity-auth-token or leave blank to be anonymous user
+    useCdn: true, // `false` if you want to ensure fresh data
+})
+
+module.exports = async function () {
+    const query = `
+    {'posts': *[ _type == "post"]{
+      title,
+      excerpt,
+      author,
+      mainImage,
+      slug { current },
+      publishedAt,
+      body
+    } | order(publishedAt desc)}
+  `
+    const params = {}
+
+    return await client.fetch(query, params)
+}
