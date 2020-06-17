af8859834e457517bc09ccb8638738c053f5b920
paweltar <paweltar@gmail.com>
2020-06-17 11:59:36 +0200

    Serve cropped images from Sanity

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,16 @@
 const blocksToHtml = require('@sanity/block-content-to-html');
+const sanityClient = require('./source/utils/sanityClient');
+const imageUrlBuilder = require('@sanity/image-url');
+
+// Get a pre-configured url-builder from your sanity client
+const builder = imageUrlBuilder(sanityClient);
+
+// Then we like to make a simple function like this that gives the
+// builder an image and returns the builder for you to specify additional
+// parameters:
+function urlFor(source) {
+    return builder.image(source)
+}
 
 module.exports = function (eleventyConfig) {
     // Copy `assets/` to `_site/assets`
@@ -21,6 +33,22 @@ module.exports = function (eleventyConfig) {
             blocks: value,
         })
     });
+
+    // Sanity Image URL Builder filter for normal cards https://www.sanity.io/docs/presenting-images
+    eleventyConfig.addFilter("urlForCard", function (sourceUrl) {
+        return urlFor(sourceUrl).width(600).auto('format').url();
+    });
+
+    // Sanity Image URL Builder filter for wide cards
+    eleventyConfig.addFilter("urlForWideCard", function (sourceUrl) {
+        return urlFor(sourceUrl).width(650).auto('format').url();
+    });
+
+    // Sanity Image URL Builder filter for case study cover images
+    eleventyConfig.addFilter("urlForCoverImg", function (sourceUrl) {
+        return urlFor(sourceUrl).width(900).auto('format').url();
+    });
+
 
     // You can return your Config object (optional).
     return {
